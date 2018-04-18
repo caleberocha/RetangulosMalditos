8a6b586b2d8ad9f9509111094b9c097bdbac80cd
Calebe <angelo.calebe@acad.pucrs.br>
2018-04-18 15:42:32 -0300

    Verificação do Java no run.bat

--- run.bat
+++ run.bat
@@ -1,13 +1,23 @@
 @echo off
 pushd "%~dp0"
-if not exist build/retangulos.jar (
-	javac -d bin src\pucrs\alest2\t1\*.java
-	jar cfe build\retangulos.jar pucrs.alest2.t1.Main -C bin pucrs
+if exist build/retangulos.jar goto _Run
+set javac=
+for /f %%a in (javac.exe) do set javac=%%$~PATH:a
+if "%javac%"=="" (
+	echo javac nao encontrado. Instale o JDK.
+	goto _End
 )
 
+javac -d bin src\pucrs\alest2\t1\*.java
+jar cfe build\retangulos.jar pucrs.alest2.t1.Main -C bin pucrs
+
+:_Run
 for /r %%a in (data/teste*) do (
 	echo %%~nxa
 	java -jar build\retangulos.jar "%%~a"
 	echo:
 )
+
+:_End
 popd
+
